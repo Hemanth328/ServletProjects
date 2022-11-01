84354a80faef13b3e716f2db0712e4cdad39b57e
dev328 <dev11@gmail.com>
2022-11-02 05:01:23 +0530

    Actual Business logics have been  implemented.

--- LinksApp/src/main/java/com/hem/servlet/LinksServlet.java
+++ LinksApp/src/main/java/com/hem/servlet/LinksServlet.java
@@ -2,6 +2,7 @@ package com.hem.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Locale;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -20,6 +21,28 @@ public class LinksServlet extends HttpServlet {
 		
 		//read the "p1" request param value
 		String p1val = req.getParameter("p1");
+		
+		// differentiate the logics for hyperlinks
+		Locale locales[] = Locale.getAvailableLocales();
+		if(p1val.equalsIgnoreCase("link1")) {
+			pw.println("<h2>All Countries</h2><br>");
+			for(Locale l: locales) {
+				pw.println(l.getDisplayCountry()+",");
+			}
+		}
+		else if(p1val.equalsIgnoreCase("link2")) {
+			pw.println("<h2> All Languages</h2><br>");
+			for(Locale l: locales) {
+				pw.println(l.getDisplayLanguage());
+			}
+		}
+		else {
+			pw.println("<h2> System Properties</h2><br>");
+			pw.println(System.getProperties());
+		}
+		
+		
+		pw.println("<br><a href='links.html'> Home </a>");
 	}
 	
 	@Override
